aa820541e30274544cab8b9010b816b4be942266
Dfingerhut <d.fingerhut38@gmail.com>
2025-01-03 20:20:44 -0700

    modified:   Flask/Web Dev/Habit Tracker/__pycache__/app.cpython-313.pyc 	modified:   Flask/Web Dev/Habit Tracker/__pycache__/routes.cpython-313.pyc 	modified:   Flask/Web Dev/microblog/__pycache__/app.cpython-313.pyc 	modified:   Flask/Web Dev/microblog/app.py

--- Flask/Web Dev/microblog/app.py
+++ Flask/Web Dev/microblog/app.py
@@ -4,26 +4,29 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-def create_app():
-    app = Flask(__name__)
-    client = MongoClient("MONGODB_URI")
-    app.db = client.microblog
+load_dotenv() 
 
-    @app.route("/", methods=["GET", "POST"])
-    def home():
-        if request.method == "POST":
-            entry_content = request.form.get("content")
-            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
-        
-        entries_with_date = [
-            (
-                entry["content"],
-                entry["date"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-            )
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html", entries=entries_with_date)
-    
-    return app
+
+app = Flask(__name__)
+client = MongoClient(os.environ.get("MONGODB_URI"))
+app.db = client.get_default_database()
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        # Get top 3 recent entries, sorted by date in descending order
+    entries_with_date = [
+        (
+            entry["content"],
+            entry["date"],                
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({}).sort([("date", -1), ("_id", -1)]).limit(3) # Sort by date descending and limit to 3
+    ]
+    return render_template("home.html", entries=entries_with_date)
+
